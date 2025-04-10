7a998840f7fbb212333d02414f6053fde61230b0
Erik Ondruš <ondrus.eodesign@gmail.com>
2025-04-10 20:32:38 +0200

    ✅ Created 1st workflow: YT Transcript, Ai Summary

---
+++ convex/workflow.ts
@@ -0,0 +1,35 @@
+import { v } from "convex/values";
+
+import { internal } from "./_generated/api";
+import { workflow } from ".";
+import { mutation } from "./_generated/server";
+
+export const kickoffTitleGenerationWorkflow = mutation({
+  args: { url: v.string() },
+  handler: async (ctx, args) => {
+    await workflow.start(ctx, internal.workflow.generateTitleWorkflow, {
+      url: args.url,
+    });
+  },
+});
+
+export const generateTitleWorkflow = workflow.define({
+  args: { url: v.string() },
+  handler: async (step, args) => {
+    const transcript: string = await step.runAction(
+      internal.transcripts.getYoutubeTranscript,
+      {
+        url: args.url,
+      }
+    );
+
+    const summary: string = await step.runAction(
+      internal.transcripts.generateSummary,
+      {
+        transcript: transcript,
+      }
+    );
+
+    return summary;
+  },
+});
